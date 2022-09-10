be8a98eb41041a2c379fba8c031565220e00b40c
e.camalzade <e.camalzade@STHQ0116-01>
2022-09-10 17:43:02 +0400

    Add project files.

---
+++ WpfApp2/MainWindow.xaml.cs
@@ -0,0 +1,109 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public string ImagePath { get; set; }
+        public string Minute { get; set; }
+        public string Description { get; set; }
+
+        public dynamic Data { get; set; }
+
+        public dynamic SingleData { get; set; }
+
+        HttpClient httpClient = new HttpClient();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            //var name = movieTextBox.Text;
+            //HttpResponseMessage response = new HttpResponseMessage();
+            //response = httpClient.GetAsync($@"http://www.omdbapi.com/?apikey=ddee1dae&s={name}&plot=full").Result;
+
+
+            //var str = response.Content.ReadAsStringAsync().Result;
+            //Data = JsonConvert.DeserializeObject(str);
+
+
+            //response = httpClient.GetAsync($@"http://www.omdbapi.com/?apikey=ddee1dae&t={Data.Search[0].Title}&plot=full").Result;
+
+
+            //str = response.Content.ReadAsStringAsync().Result;
+            //SingleData = JsonConvert.DeserializeObject(str);
+
+
+            //movieImage.Source = SingleData.Poster;
+            //movieImage2.Source = SingleData.Poster;
+            //Minute = SingleData.Runtime;
+            //Description = SingleData.Description;
+
+            //movieLabel.Content = Minute + "  " + Description;
+
+
+
+            var name = movieTextBox.Text;
+            HttpResponseMessage response = new HttpResponseMessage();
+            response = httpClient.GetAsync($@"http://www.omdbapi.com/?apikey=ddee1dae&s={name}&plot=full").Result;
+
+
+            var str = response.Content.ReadAsStringAsync().Result;
+            Data = JsonConvert.DeserializeObject(str);
+
+
+            response = httpClient.GetAsync($@"http://www.omdbapi.com/?apikey=ddee1dae&t={Data.Search[0].Title}&plot=full").Result;
+
+
+            str = response.Content.ReadAsStringAsync().Result;
+            SingleData = JsonConvert.DeserializeObject(str);
+
+
+            movieImage.Source = SingleData.Poster;
+            movieImage2.Source = SingleData.Poster;
+            Minute = SingleData.Runtime;
+            Description = SingleData.Description;
+
+            movieLabel.Content = Minute + "  " + Description;
+
+            response = httpClient.GetAsync($@"http://www.omdbapi.com/?apikey=ddee1dae&t={Data.Search[1].Title}&plot=full").Result;
+
+
+            str = response.Content.ReadAsStringAsync().Result;
+            SingleData = JsonConvert.DeserializeObject(str);
+
+
+            movieImage11.Source = SingleData.Poster;
+            movieImage12.Source = SingleData.Poster;
+            Minute = SingleData.Runtime;
+            Description = SingleData.Description;
+
+            movieLabel.Content = Minute + "  " + Description;
+
+
+        }
+    }
+}
